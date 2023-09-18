fdf355234290d994ef1b6ee124b84a77528a18e1
Jameson Piscitello <piscitello284@gmail.com>
2023-09-18 11:52:06 -0500

    change it

--- main.py
+++ main.py
@@ -1,34 +1,84 @@
-#setup
-import pygame
-from pygame.locals import *
-pygame.init()
-#vars
-s_width =1000
-s_height = 1000
-screen= pygame.display.set_mode((s_width,s_height))
-pygame.display.set_caption("Platfomer")
+from typing import Tuple
 
-sun = pygame.image.load("img/sun.png")
-sky = pygame.image.load("img/sky.jpg")
-player = pygame.Rect((300,250,50,50))
-#loop
-run=True
-while run:
-    screen.fill((0,0,0))
-    screen.blit(sky,(0,0))
-    screen.blit(sun,(100,100))
-    pygame.draw.rect(screen, (255,0,0),player)
-    key=pygame.key.get_pressed()
-    if key[pygame.K_a]== True:
-        player.move_ip(-1,0)
-    elif key[pygame.K_w]== True:
-        player.move_ip(0,-1)
-    elif key[pygame.K_s]== True:
-        player.move_ip(0,1)
-    elif key[pygame.K_d]== True:
-        player.move_ip(1,0)
+import pygame
+import sys
+import os
+
+'''
+Variables
+'''
+
+worldx = 960
+worldy = 720
+fps = 40  # frame rate
+ani = 4  # animation cycles
+world = pygame.display.set_mode([worldx, worldy])
+
+BLUE = (25, 25, 200)
+BLACK = (23, 23, 23)
+WHITE = (254, 254, 254)
+ALPHA = (0, 255, 0)
+
+'''
+Objects
+'''
+
+
+class Player(pygame.sprite.Sprite):
+    """
+    Spawn a player
+    """
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.images = []
+        for i in range(1, 5):
+            img = pygame.image.load(os.path.join('images', 'hero' + str(i) + '.png')).convert()
+            img.convert_alpha()  # optimise alpha
+            img.set_colorkey(ALPHA)  # set alpha
+            self.images.append(img)
+            self.image = self.images[0]
+            self.rect = self.image.get_rect()
+
+
+'''
+Setup
+'''
+
+backdrop = pygame.image.load(os.path.join('images', 'stage.png'))
+clock = pygame.time.Clock()
+pygame.init()
+backdropbox = world.get_rect()
+main = True
+
+player = Player()  # spawn player
+player.rect.x = 0  # go to x
+player.rect.y = 0  # go to y
+player_list = pygame.sprite.Group()
+player_list.add(player)
+
+
+'''
+Main Loop
+'''
+
+while main:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            run=False
-    pygame.display.update()
-pygame.quit()
+            pygame.quit()
+            try:
+                sys.exit()
+            finally:
+                main = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == ord('q'):
+                pygame.quit()
+            try:
+                sys.exit()
+            finally:
+                main = False
+    world.blit(backdrop, backdropbox)
+    player_list.draw(world)
+    pygame.display.flip()
+    clock.tick(fps)
